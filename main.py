66ee52e12724aeaab97e401e6188e0c433518df2
Yesaya Pranadinata <yesayapranadinata@gmail.com>
2021-08-05 11:36:07 +0700

    upload pertama

---
+++ main.py
@@ -0,0 +1,19 @@
+
+#dasar dasar python
+print("Hello World");
+
+#variable 
+nama = 'Yesaya Pranadinata'
+umur = 23
+
+print(nama)             #mencetak nilai nama
+type(nama)
+print(umur)             #mencetak nilai umur
+
+#proses aritmatika
+panjang = 10
+lebar = 5 
+luas = panjang * lebar
+
+print(luas)
+
